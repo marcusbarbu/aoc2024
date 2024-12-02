511a2b0e1f235cc5a866a656f32ca78720a27bc3
marcus <mgb.barbu@gmail.com>
2024-12-01 18:51:39 -0800

    refactored for some helper functions, code cleanup

--- days/day1/src/main.rs
+++ days/day1/src/main.rs
@@ -1,73 +1,68 @@
-use std::{collections::BTreeMap, env, iter::zip};
+use aoc2024::{AocHelper, RequestedAocInputType};
+use std::{collections::BTreeMap, iter::zip};
 struct Day1PartA {
-    input_path: String,
+    raw: String,
     a_vec: Vec<i32>,
-    b_vec: Vec<i32>
+    b_vec: Vec<i32>,
 }
 
 impl Day1PartA {
-    pub fn new(path: &str) -> Self {
-        let p: String = path.to_string();
-        Day1PartA{
-            input_path: p,
+    pub fn new(raw: &String) -> Self {
+        Day1PartA {
+            raw: raw.clone(),
             a_vec: Vec::new(),
-            b_vec: Vec::new()
+            b_vec: Vec::new(),
         }
     }
 
-    pub fn parse(&mut self){
-        let raw = std::fs::read_to_string(self.input_path.clone());
-        let raw = raw.unwrap();
-
-        raw.lines().for_each(|line| {
+    pub fn parse(&mut self) {
+        self.raw.lines().for_each(|line| {
             let mut parts = line.split_ascii_whitespace();
-            self.a_vec.push(parts.next().unwrap().parse::<i32>().unwrap());
-            self.b_vec.push(parts.next().unwrap().parse::<i32>().unwrap());
+            self.a_vec
+                .push(parts.next().unwrap().parse::<i32>().unwrap());
+            self.b_vec
+                .push(parts.next().unwrap().parse::<i32>().unwrap());
         });
-
     }
 
-    pub fn sort_lists(&mut self){
+    pub fn sort_lists(&mut self) {
         self.a_vec.sort();
         self.b_vec.sort();
     }
 
-    pub fn get_diff_sum(&mut self) -> i32{
-
-        let ans: i32 = zip(&mut self.a_vec, &mut self.b_vec).fold(0, |acc, (a, b)| {
-            acc + a.abs_diff(*b) as i32
-        });
+    pub fn get_diff_sum(&mut self) -> i32 {
+        let ans: i32 = zip(&mut self.a_vec, &mut self.b_vec)
+            .fold(0, |acc, (a, b)| acc + a.abs_diff(*b) as i32);
         ans
     }
 }
 
 struct Day1PartB {
-    input_path: String,
+    raw: String,
     a_vec: Vec<i32>,
-    b_counter: BTreeMap<i32, u32>
+    b_counter: BTreeMap<i32, u32>,
 }
 
 impl Day1PartB {
-    pub fn new(path: &str) -> Self {
-        let p: String = path.to_string();
-        Day1PartB{
-            input_path: p,
+    pub fn new(raw: &String) -> Self {
+        Day1PartB {
+            raw: raw.clone(),
             a_vec: Vec::new(),
-            b_counter: BTreeMap::new()
+            b_counter: BTreeMap::new(),
         }
     }
 
-    pub fn parse(&mut self){
-        let raw = std::fs::read_to_string(self.input_path.clone());
-        let raw = raw.unwrap();
-
-        raw.lines().for_each(|line| {
+    pub fn parse(&mut self) {
+        self.raw.lines().for_each(|line| {
             let mut parts = line.split_ascii_whitespace();
-            self.a_vec.push(parts.next().unwrap().parse::<i32>().unwrap());
+            self.a_vec
+                .push(parts.next().unwrap().parse::<i32>().unwrap());
             let b = parts.next().unwrap().parse::<i32>().unwrap();
-            self.b_counter.entry(b).and_modify(|cur| *cur += 1).or_insert(1);
+            self.b_counter
+                .entry(b)
+                .and_modify(|cur| *cur += 1)
+                .or_insert(1);
         });
-
     }
 
     pub fn get_score(&self) -> i32 {
@@ -76,41 +71,40 @@ impl Day1PartB {
             acc + *cur * cur_score
         })
     }
-
 }
 
-
-
-
 fn main() {
-    let _ = dotenvy::dotenv();
-    let base_path: String = env::var("STATIC_BASE_PATH").unwrap();
-    let test_path = base_path.clone() + "/day1/test_input";
-    let mut day1_part_a_test: Day1PartA = Day1PartA::new(&test_path);
+    let aoc: AocHelper = AocHelper::new(1, None);
+    let test_input: String = aoc
+        .get_input_as_string(RequestedAocInputType::Test)
+        .expect("Failed to read day1 test input");
+    let real_input: String = aoc
+        .get_input_as_string(RequestedAocInputType::Real)
+        .expect("Failed to read day1 real input");
+
+    let mut day1_part_a_test: Day1PartA = Day1PartA::new(&test_input);
     day1_part_a_test.sort_lists();
     day1_part_a_test.parse();
 
     let ans = day1_part_a_test.get_diff_sum();
     println!("Found diff answer: {}", ans);
 
-    let real_path = base_path + "/day1/real_input";
-    let mut day1_part_a_real: Day1PartA = Day1PartA::new(&real_path);
+    let mut day1_part_a_real: Day1PartA = Day1PartA::new(&real_input);
     day1_part_a_real.parse();
     day1_part_a_real.sort_lists();
 
     let ans = day1_part_a_real.get_diff_sum();
     println!("Found diff answer: {}", ans);
 
-    let mut day1_partb_test: Day1PartB = Day1PartB::new(&test_path);
+    let mut day1_partb_test: Day1PartB = Day1PartB::new(&test_input);
     day1_partb_test.parse();
     println!("Counter: {:?}", day1_partb_test.b_counter);
     let ans = day1_partb_test.get_score();
     println!("Score: {}", ans);
 
-    let mut day1_partb_real: Day1PartB = Day1PartB::new(&real_path);
+    let mut day1_partb_real: Day1PartB = Day1PartB::new(&real_input);
     day1_partb_real.parse();
     println!("Counter: {:?}", day1_partb_real.b_counter);
     let ans = day1_partb_real.get_score();
     println!("Score: {}", ans);
-
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,131 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use core::fmt;
+use std::env;
+use std::path::{Path, PathBuf};
+
+use tracing::{error, Level};
+
+#[derive(Debug, Clone)]
+pub enum RequestedAocInputType {
+    Real,
+    Test,
+    CustomTest { fname: String },
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct AocHelper {
+    _day: u32,
+    test_inputs: Vec<PathBuf>,
+    real_input: PathBuf,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Debug, Clone)]
+pub enum AocHelperError {
+    FileReadError,
+}
+
+type AocResult<T> = std::result::Result<T, AocHelperError>;
+
+impl fmt::Display for AocHelperError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match *self {
+            AocHelperError::FileReadError => {
+                write!(f, "File read error! Could not read file!")
+            }
+        }
+    }
+}
+
+impl AocHelper {
+    fn setup_logging() {
+        let subscriber = tracing_subscriber::FmtSubscriber::builder()
+            .with_max_level(Level::DEBUG)
+            .with_thread_ids(true)
+            .with_thread_names(true)
+            .with_ansi(true)
+            .finish();
+
+        let _ = tracing::subscriber::set_global_default(subscriber);
+    }
+
+    pub fn new(day: u32, addl_test_inputs: Option<Vec<String>>) -> Self {
+        AocHelper::setup_logging();
+        let _ = dotenvy::dotenv();
+        let base_path: String = env::var("STATIC_BASE_PATH").unwrap();
+        let mut day_input: PathBuf = PathBuf::new();
+        day_input.push(base_path);
+        day_input.push(format!("day{}", day));
+
+        let mut real_input: PathBuf = day_input.clone();
+        real_input.push("real_input");
+
+        let mut test_inputs: Vec<PathBuf> = Vec::new();
+        let mut default_test_input: PathBuf = day_input.clone();
+        default_test_input.push("test_input");
+        test_inputs.push(default_test_input);
+
+        match addl_test_inputs {
+            Some(input_fnames) => {
+                input_fnames.into_iter().for_each(|fname| {
+                    let mut new_test_path: PathBuf = day_input.clone();
+                    new_test_path.push(fname);
+                    test_inputs.push(new_test_path);
+                });
+            }
+            None => {}
+        };
+
+        AocHelper {
+            _day: day,
+            test_inputs: test_inputs,
+            real_input: real_input,
+        }
+    }
+
+    pub fn get_real_input_path(&self) -> PathBuf {
+        self.real_input.clone()
+    }
+
+    pub fn get_test_input_path(&self, fname: Option<&str>) -> Option<PathBuf> {
+        if let Some(target) = fname {
+            return self
+                .test_inputs
+                .iter()
+                .find(|buf| buf.ends_with(target))
+                .cloned();
+        }
+        return self.test_inputs.iter().next().cloned();
+    }
+
+    fn get_fname_as_string(fname: &PathBuf) -> AocResult<String> {
+        let data = std::fs::read_to_string(&fname);
+        match data {
+            Ok(d) => return Ok(d),
+            Err(e) => {
+                error!(
+                    "Failed to read file {}, err: {}",
+                    fname.to_str().unwrap_or("BAD_FNAME"),
+                    e
+                );
+                return Err(AocHelperError::FileReadError);
+            }
+        }
+    }
+
+    pub fn get_input_as_string(&self, input_type: RequestedAocInputType) -> AocResult<String> {
+        match input_type {
+            RequestedAocInputType::Real => {
+                return AocHelper::get_fname_as_string(&self.real_input);
+            }
+            RequestedAocInputType::Test => {
+                return AocHelper::get_fname_as_string(&self.test_inputs.iter().next().unwrap());
+            }
+            RequestedAocInputType::CustomTest { fname } => {
+                let fname = self.get_test_input_path(Some(fname.as_str()));
+                if let Some(pb) = fname {
+                    return AocHelper::get_fname_as_string(&pb);
+                }
+            }
+        }
+
+        Err(AocHelperError::FileReadError)
     }
 }
